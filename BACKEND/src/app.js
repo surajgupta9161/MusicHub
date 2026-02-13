cdba46ff8a6f71f8c2a701f5756412dc963ae508
surajgupta <suraj666189@gmail.com>
2026-02-13 14:44:49 +0530

    adding frontend live url

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -9,10 +9,12 @@ const cors = require('cors')
 const app = express()
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://musichub-2.onrender.com',
     credentials: true
   })
 )
+
+// http://localhost:5173
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
